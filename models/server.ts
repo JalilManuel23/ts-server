7c41b9f3d578db3655dce95128d7d7befb0b3fcb
jalilmanuel25 <jalilmanuel25@gmail.com>
2022-07-23 14:40:27 -0500

    Definir rutas

--- models/server.ts
+++ models/server.ts
@@ -1,12 +1,20 @@
 import express, { Application } from 'express';
+import userRouter from '../routes/usuario';
 
 class Server {
     private app: Application;
     private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    }
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8000';
+    }
+
+    routes() {
+        this.app.use( this.apiPaths.usuarios, userRouter );
     }
 
     listen() {
